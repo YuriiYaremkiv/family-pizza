343915df0fdca73df7ef71f0b9e7f8f8b311a79e
yyaremkiv <y.yaremkiv@gmail.com>
2023-04-02 21:55:47 +0300

    update

--- data.js
+++ data.js
@@ -1,40 +1,26 @@
 export const data = ({ name, email, phone, orders, price }) => {
-  return `
-  <html>
-      <head>
-      <style>
-      body {
-        font-family: Arial, sans-serif;
-            font-size: 16px;
-            line-height: 1.5;
-          }
-          h1 {
-            font-size: 24px;
-            margin-bottom: 20px;
-          }
-          p {
-            margin-bottom: 20px;
-          }
-          .cta-btn {
-            display: inline-block;
-            padding: 10px 20px;
-            background-color: #F15A29;
-            color: #fff;
-            text-decoration: none;
-            border-radius: 5px;
-          }
-          .cta-btn:hover {
-            background-color: #D44E22;
-          }
-          </style>
-          </head>
-          <body>
-          <h1>Sending with SendGrid is Fun</h1>
-          <p>Name ${name},</p>
-          <p>Email ${email},</p>
-          <p>Phone ${phone},</p>
-          <p>Price ${price},</p>
-          </body>
-          </html>
-          `;
+  return `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Pizza Order Confirmation</title>
+  </head>
+  <body>
+    <div style="background-color: #f2f2f2; padding: 20px;">
+      <h1 style="text-align: center; color: #ff9800;">Pizza Order Confirmation</h1>
+      <p>Dear ${name},</p>
+      <p>Thank you for your order! We have received the following information:</p>
+      <ul>
+        <li><strong>Name:</strong> ${name}</li>
+        <li><strong>Email:</strong> ${email}</li>
+        <li><strong>Phone:</strong> ${phone}</li>
+        <li><strong>Total Price:</strong> $${price}</li>
+      </ul>
+      <p>We will begin processing your order immediately and will send you a confirmation email when it is ready for pickup. If you have any questions or concerns, please don't hesitate to contact us at 1-800-PIZZA.</p>
+      <p>Thank you for choosing our pizzeria!</p>
+      <p>Sincerely,<br />The Pizza Team</p>
+    </div>
+  </body>
+</html>`;
 };
